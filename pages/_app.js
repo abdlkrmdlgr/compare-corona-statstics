9f49a79157310856f2e650140cea45da1138a946
Borte Coder <abdulkerimdulger@hotmail.com>
2020-04-16 17:43:01 +0300

    graph screen 500 error fix

--- pages/_app.js
+++ pages/_app.js
@@ -11,33 +11,35 @@ class CustomApp extends App {
     }
 
     componentDidMount() {
-        $(function () {
-            // $(document).on("click", ".countryBadge", function (e) {
-            //     var item = $($(".country .countryNameLabel:contains(" + e.target.innerText.replace("#", "") + ")")[0].parentElement);
-            //     item.removeClass("bg-info");
-            //     item.removeClass("text-white");
-            //     item.addClass("text-danger");
-            //     $(".badge:contains(" + e.target.innerText + ")").remove();
-            // });
-            //
-            // if ($(".countries") != null) {
-            //     $(".countries").css("padding-top", $(".navbar").height() + "px");
-            //     $("#countryNameId").focus();
-            // }
-            //
-            // if ($(".chartGroupButton") != null) {
-            //     $(".chartGroupButton button")[0].click();
-            // }
+        // $(function () {
+        //     // $(document).on("click", ".countryBadge", function (e) {
+        //     //     var item = $($(".country .countryNameLabel:contains(" + e.target.innerText.replace("#", "") + ")")[0].parentElement);
+        //     //     item.removeClass("bg-info");
+        //     //     item.removeClass("text-white");
+        //     //     item.addClass("text-danger");
+        //     //     $(".badge:contains(" + e.target.innerText + ")").remove();
+        //     // });
+        //     //
+        //     // if ($(".countries") != null) {
+        //     //     $(".countries").css("padding-top", $(".navbar").height() + "px");
+        //     //     $("#countryNameId").focus();
+        //     // }
+        //     //
+        //     // if ($(".chartGroupButton") != null) {
+        //     //     $(".chartGroupButton button")[0].click();
+        //     // }
+        //
+        //
+        // });
 
-            window.dataLayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];
 
-            function gtag() {
-                dataLayer.push(arguments);
-            }
+        function gtag() {
+            dataLayer.push(arguments);
+        }
 
-            gtag('js', new Date());
-            gtag('config', 'UA-12370199-5');
-        });
+        gtag('js', new Date());
+        gtag('config', 'UA-12370199-5');
     }
 }
 
